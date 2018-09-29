b8996465a15eb5bcc17a3f56e271b4f6f08b88ae
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-09-29 12:29:29 -0400

    backup

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ $(function() {
 //     solo.css('height', width);
 // });
 
-$(function() {
-    setTimeout(() => {
-        var bird = $(".bird")[0];
-        console.log(bird)
-        bird.parentNode.removeChild(bird);
-    }, 15000);
-})
+// $(function() {
+//     setTimeout(() => {
+//         var bird = $(".bird")[0];
+//         console.log(bird)
+//         bird.parentNode.removeChild(bird);
+//     }, 15000);
+// })
